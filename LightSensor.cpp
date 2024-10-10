ca2b93e3c48f76ab932af868f97465fc6f534669
OnealSel <landrygohui@gmail.com>
2024-10-10 16:34:00 +0200

    commit

--- LightSensor.cpp
+++ LightSensor.cpp
@@ -7,3 +7,12 @@ std::string LightSensor::generateData() {
     bool lightOn = rand() % 2;
     return "Light Sensor ID " + std::to_string(id) + ": " + (lightOn ? "ON" : "OFF");
 }
+
+
+
+std::string LightSensor::generateData() {
+    bool lightOn = rand() % 2;
+    std::string data = "Light Sensor ID " + std::to_string(id) + ": " + (lightOn ? "ON" : "OFF");
+    server->fileWrite("Light_log.txt", data); // Écriture dans le fichier de log
+    return data;
+}
--- TemparatureSensor.cpp
+++ TemparatureSensor.cpp
@@ -7,3 +7,4 @@ std::string TemparatureSensor::generateData() {
     float temperature = 15.0f + static_cast<float>(rand()) / (static_cast<float>(RAND_MAX / (25.0f - 15.0f)));
     return "Temperature Sensor ID " + std::to_string(id) + ": " + std::to_string(temperature) + " °C";
 }
+//hfb"àrhufvb"rh
